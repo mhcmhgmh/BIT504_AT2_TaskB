12e85daed9b95d61524360b101e3938d3e2c293a
Simon Hankins <30307579+mhcmhgmh@users.noreply.github.com>
2022-01-11 13:26:57 +1300

    Updated Ball Width
    Changed Ball Width to 40 in Ball.java code.

--- Pong/src/Ball.java
+++ Pong/src/Ball.java
@@ -1,7 +1,7 @@
 import java.awt.Color;
 
 public class Ball extends Sprite {
-	       private static final int BALL_WIDTH = 30;
+	       private static final int BALL_WIDTH = 40;
 	       private static final int BALL_HEIGHT = 30;
 	       private static final Color BALL_COLOUR = Color.WHITE;
 	   
